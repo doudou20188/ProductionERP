9fe377fc91e8c02640c9cf69d8309d0ab16d60a1
lyboy0414 <lyboy0414@163.com>
2018-12-06 16:36:32 +0800

    可运行

---
+++ ProductionERP/src/main/java/com/cskaoyan/Controller/mainController.java
@@ -0,0 +1,20 @@
+package com.cskaoyan.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Demo class
+ *
+ * @Author lyboy
+ * @Date 2018/12/6 12:39
+ **/
+@Controller
+public class mainController {
+    @RequestMapping("/{name}")
+    public String main(@PathVariable String name){
+        System.out.println(name);
+        return name;
+    }
+}
