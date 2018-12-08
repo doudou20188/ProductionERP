5a73623b4c2f95d1b2c01dd7b50a6da4308ca9f3
lyboy0414 <lyboy0414@163.com>
2018-12-08 10:02:18 +0800

    合并---李（device V1.5）

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,6 @@
-import com.cskaoyan.domain.DeviceTypeListVO;
-import com.cskaoyan.service.DeviceTypeService;
+import com.cskaoyan.domain.Device.DeviceListVO;
+import com.cskaoyan.mapper.Device.DeviceMapper;
+import com.cskaoyan.service.Devicr.DeviceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +18,40 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ContextConfiguration("classpath:applicationContext.xml")
 @WebAppConfiguration
 public class MyTest {
-   /* @Autowired
-    DeviceTypeMapper deviceTypeMapper;*/
-
-
     @Autowired
-    DeviceTypeService deviceTypeService;
+    DeviceMapper deviceMapper;
+    @Autowired
+    DeviceService deviceService;
+
+
+    //    @Autowired
+//    DeviceTypeService deviceTypeService;
+//    @Test
+//    public void Test1(){
+//      /*  List<DeviceType> devices = deviceTypeMapper.deviceTypeList();
+//        System.out.println(devices);*/
+//    }
+//    @Test
+//    public void Test2(){
+//        DeviceTypeListVO deviceTypeListVO = deviceTypeService.deviceTypeList("1", "2");
+//        System.out.println(deviceTypeListVO);
+//    }
+//    @Test
+//    public void Test3(){
+//        DeviceType deviceType =new DeviceType();
+//        deviceType.setDeviceTypeId("07");
+//        deviceType.setDeviceTypeModel("a");
+//        deviceType.setDeviceTypeProducer("a");
+//        deviceType.setDeviceTypeName("a");
+//        deviceType.setDeviceTypeQuantity("11");
+//        deviceType.setDeviceTypeSpec("s");
+//        deviceType.setDeviceTypeWarranty("2018-08-08 08:08:08");
+//       Status status=deviceTypeService.insert(deviceType);
+//        System.out.println(status);
+//    }
     @Test
-    public void Test1(){
-      /*  List<DeviceType> devices = deviceTypeMapper.deviceTypeList();
-        System.out.println(devices);*/
-    }
-    @Test
-    public void Test2(){
-        DeviceTypeListVO deviceTypeListVO = deviceTypeService.deviceTypeList("1", "2");
-        System.out.println(deviceTypeListVO);
+    public void Test4() {
+        DeviceListVO deviceListVO = deviceService.selectList("1", "10");
+        System.out.println(deviceListVO);
     }
 }
